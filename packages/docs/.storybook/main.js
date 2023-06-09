609e06dd47092bcb83b1b16ba0482666392c1c36
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2023-06-09 14:41:16 -0300

    fix production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,7 +15,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = 'jasmim-ds'
+      config.base = '/jasmim-ds/'
     }
     return config
   }
